f2bd98a1235a4d4ca184fd4b4111c2c5279e7e86
Rayanne Moraes <moraesrayanne@hotmail.com>
2021-03-09 18:05:44 -0300

    finalizando

---
+++ componentes/concluiTarefa.js
@@ -0,0 +1,16 @@
+const BotaoConcluir = () => {
+    const botaoConcluir = document.createElement('button');
+    botaoConcluir.classList.add('check-button');
+    botaoConcluir.innerText = 'Concluir';
+    botaoConcluir.addEventListener('click', concluirTarefa);
+    return botaoConcluir;
+}
+
+const concluirTarefa = (evento) => {;
+    const botaoConclui = evento.target;
+    const tarefaCompleta = botaoConclui.parentElement;
+
+    tarefaCompleta.classList.toggle('done');
+}
+
+export default BotaoConcluir;
